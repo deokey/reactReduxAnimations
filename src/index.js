94d187d01954e80bce2c052a014adeb0369c6314
Ibsen Gonzalez <ibsenjg@gmail.com>
2018-10-24 16:14:31 -0500

    Feat: integracion sencilla de redux en el proyecto

--- src/index.js
+++ src/index.js
@@ -3,13 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import cheet from 'cheet.js';
 
+// Para implementar redux
+// 1. importamos el provider que servira para encapsular a nuestra app para que lleve la data a todo su content
+import { Provider } from 'react-redux';
+
+// 2. importamos el store ya que nuestro componente provider necesita un store para almacenar su data.
+import { createStore } from 'redux';
+
+/* se importa cheetjs y se llama su funcion para que podamos trabajar con su funcionalidad desde todo el
+marco del proyecto */
+import cheet from 'cheet.js';
 cheet('a s d', () => {
   alert('yes baby');
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 4. creamos un initialState
+const initialState = {};
+
+// 5. creamos un reducer
+// es una funcion que se usa para hacer cambios al estado
+// reducer(state,action)
+function reducer(state, action) {
+  switch (action.type) {
+    case 'UPDATE_PROPS': {
+      const newProps = action.payload.props;
+      return {
+        ...state,
+        ...newProps
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+// 3. instanciamos el store y lo agregamos al provider como atributo
+// createStore(reducer, initialState)
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
